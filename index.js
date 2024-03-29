9940f4f91dde2e69f0cab6c0d1be885e3457c841
HalimawMagSilos <164495748+HalimawMagSilos@users.noreply.github.com>
2024-03-29 21:46:57 +0800

    Add files via upload
    function

---
+++ index.js
@@ -0,0 +1,6 @@
+function displayText() {
+    // Get the element where you want to display the text
+    var displayElement = document.getElementById("display-area");
+    // Set the text content
+    displayElement.textContent = "I am Programmer";
+}
